fcadbb84c72fd67e6fa48bdabc9dfba9294015cf
freeAbrams <iamsc2_master@outlook.com>
2024-07-18 13:30:39 +0900

    update comment

--- seismic_shield/config.py
+++ seismic_shield/config.py
@@ -2,8 +2,8 @@
 
 API_URL = "https://api-v2-sandbox.p2pquake.net/v2/history?codes=556&limit=3"
 SCALE_THRESHOLD = 30
-CHECK_INTERVAL = 3  # 以秒为单位
+CHECK_INTERVAL = 3  # seconds
 #LOG_FILE = "/var/log/seismic_shield.log"
 LOG_FILE = "seismic_shield.log"
 TARGET_AREA_NAME = "千葉県北西部"
-HARD_DRIVE_PATH = "/dev/sda"
+HARD_DRIVE_PATH = "/dev/sda" # Path to the hard drive to stop
